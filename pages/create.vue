f1a61e0fa002a289e432d77df3da3ab33285c8b3
hamid reza zare pour <hamidreza.num1@gmail.com>
2019-11-02 16:22:11 +0330

    Initial project

---
+++ pages/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Events</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create an event in real world application'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Events</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  }
+}
+</script>
