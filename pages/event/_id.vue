5d4f5231a8e29e640ea060ba1084590fcfe55cab
hamid reza zare pour <hamidreza.num1@gmail.com>
2019-11-03 11:27:27 +0330

    Using async/await and test progressbar

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Event: #{{ id }}</h1>
+  <h1>{{ event.title }}</h1>
 </template>
 
 
@@ -7,19 +7,29 @@
 export default {
   head() {
     return {
-      titleTemplate: 'Event #' + this.id,
+      titleTemplate: 'Event: ' + this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: 'What you need to know about event #' + this.id
+          content: 'What you need to know about event:' + this.event.title
         }
       ]
     }
   },
-  computed: {
-    id() {
-      return this.$route.params.id
+  async asyncData({ $axios, error, params }) {
+    try {
+      const { data } = await $axios.get(
+        'http://localhost:3030/events/' + params.id
+      )
+      return {
+        event: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unabel to fetch event #' + params.id
+      })
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -17,20 +17,18 @@ export default {
       title: 'Event Listing'
     }
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3030/events')
-      .then(response => {
-        return {
-          events: response.data
-        }
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3030/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unabel to fetch events at this time. Please try again'
       })
-      .catch(e => {
-        error({
-          statusCode: 503,
-          message: 'Unabel to fetch events at this time. Please try again'
-        })
-      })
+    }
   }
 }
 </script>
