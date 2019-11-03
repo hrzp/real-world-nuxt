36141b4a240f22b54ade581915889499ab7aa555
hamid reza zare pour <hamidreza.num1@gmail.com>
2019-11-03 10:59:54 +0330

    Add db.json - Add axios requsts with asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard v-for="(event, index) in events" :key="index" :event="event" :data-index="index" />
   </div>
 </template>
 
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: {
+    EventCard
+  },
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3030/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unabel to fetch events at this time. Please try again'
+        })
+      })
   }
 }
 </script>
